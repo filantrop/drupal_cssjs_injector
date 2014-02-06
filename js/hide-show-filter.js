cc0f354d140adb0d4c12201c891511f49cea87ac
Aateras <andreas@sateras.se>
2014-02-06 09:56:55 +0100

    Removed unneccessary js and css files

--- js/hide-show-filter.js
+++
@@ -1,9 +0,0 @@
-(function($) {
-    $().ready(function() {
-        var elm = $('<a href="">Visa / Dölj Filter</a>').insertAfter('#page-title');
-        elm.click(function(event){
-                 $('.views-exposed-form').toggle();
-                  event.preventDefault();
-        });
-    });
-})(jQuery);
